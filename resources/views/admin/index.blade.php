6063ca6ec943a32588fbc2cd284c2538e086abb9
swissv2 <swissv2@users.noreply.github.com>
2022-08-05 18:48:16 -0700

    Debugged the Middleware issue
    Debugged the Middleware issue by adding the middleware use case inside the Kernel.php

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,9 +2,8 @@
 
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Admin') }}
         </h2>
     </x-slot>
-
 
 </x-admin-layout>
