62316a8f7db1fccccc7bc23614e6e3e14445251d
swissv2 <swissv2@users.noreply.github.com>
2022-08-05 20:27:23 -0700

    add funky permissions
    permissions but need no further

--- database/migrations/2022_10_12_000000_create_users_table.php
+++ database/migrations/2022_10_12_000000_create_users_table.php
@@ -15,11 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            //Add foreign Key to this project
+            $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('is_admin')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
